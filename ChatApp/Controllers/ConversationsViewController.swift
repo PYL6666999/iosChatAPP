132c3abfb460b0e4828541fb85f44653bf400e06
pyl <pyl1@meitu.com>
2025-08-18 14:31:14 +0800

    add login && register check

--- ChatApp/Controllers/ConversationsViewController.swift
+++ ChatApp/Controllers/ConversationsViewController.swift
@@ -6,24 +6,37 @@
 //
 
 import UIKit
+import FirebaseAuth
 
 class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .red
+
     }
     
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         
-        let isLoggedIn = UserDefaults.standard.bool(forKey: "logged_in")
+        validateAuth()
+    }
+    
+    private func validateAuth() {
         
-        if !isLoggedIn {
+        do {
+            try Auth.auth().signOut()
+            print("成功登出")
+        } catch {
+            print("登出失败: \(error)")
+        }
+
+        if FirebaseAuth.Auth.auth().currentUser == nil {
             let vc = LoginViewController()
             let nav = UINavigationController(rootViewController: vc)
             nav.modalPresentationStyle = .fullScreen
             present(nav, animated: false)
+
         }
     }
 
